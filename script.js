f29a9677c95fe6ab1e20ad9b5d544eae3d7d8546
František Kasík <frantis3k.kasik@gmail.com>
2023-05-02 19:15:30 +0200

    Add code to switch between 3 themes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// globals
+const radioBoxes = document.querySelectorAll(".radio");
+
+// events
+radioBoxes.forEach(radio => {
+    radio.addEventListener('change', () => {
+        setTheme(getTheme());
+    })
+});
+
+// handlers
+function getTheme() {
+    for (let radio of radioBoxes) {
+        if (radio.checked) {
+            return radio.value;
+        }
+    }
+}
+
+// functions
+function setTheme(theme) {
+    document.documentElement.className = theme;
+}
